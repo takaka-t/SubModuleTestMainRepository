f63b29f6e83a191a5d1d859e2a77a6f3fa0d2dfc
‘takaka-t <86246662+takaka-t@users.noreply.github.com>
2022-04-27 08:52:08 +0900

    メインプロジェクト初期コミット

---
+++ MainProject/MainProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
